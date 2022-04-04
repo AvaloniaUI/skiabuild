789099139f529c561db33e864a0eb472fb5bfee1
James Walmsley <james@fullfat-fs.co.uk>
2022-04-04 23:38:20 +0000

    [AVALONIA] Use cmake to build library.

--- recipes/skia/layers/avalonia_skia.mk
+++ recipes/skia/layers/avalonia_skia.mk
@@ -1,12 +1,13 @@
 LAYER:=avalonia_skia
 include $(DEFINE_LAYER)
 
+avalonia_skia:=$(LSTAMP)/avalonia.skia
 avalonia_symbols:=$(PKGROOT)/libavalonia.skia.symbols
 avalonia_install:=$(LSTAMP)/avalonia_install
 
-$(call meson_srcdir, avalonia_skia, $(BASE)/libavalonia.skia)
+#$(call meson_srcdir, avalonia_skia, $(BASE)/libavalonia.skia)
 
-
+$(L) += $(avalonia_skia)
 $(L) += $(avalonia_symbols)
 $(L) += $(avalonia_install)
 
@@ -16,12 +17,16 @@ include $(BUILD_LAYER)
 
 $(L): $(BASE)/libavalonia.skia/meson.build
 
-$(avalonia_symbols): $(LSTAMP)/avalonia_skia
-	nm $(SYSROOT)/$(PREFIX)/$(LIBDIR)/libavalonia.skia.so | grep '.*\sT\s.*$$' > $@
+CMAKE_FLAGS += -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$(SYSROOT)/$(PREFIX) -DPKGROOT=$(PKGROOT) -DCMAKE_INSTALL_LIBDIR=$(LIBDIR)
 
-$(avalonia_install): $(LSTAMP)/avalonia_skia $(avalonia_symbols)
-	cd $(builddir)/avalonia_skia && DESTDIR=$(PKGROOT) ninja install
-	$(stamp)
+$(avalonia_skia):
+	mkdir -p $(builddir)/avalonia.skia
+	cd $(builddir)/avalonia.skia && cmake $(CMAKE_FLAGS) $(BASE)/libavalonia.skia
+	cd $(builddir)/avalonia.skia && ninja
+	cd $(builddir)/avalonia.skia && DESTDIR=$(PKGROOT) ninja install
+
+$(avalonia_symbols): $(avalonia_skia)
+	nm $(PKGROOT)/$(PREFIX)/$(LIBDIR)/libavalonia.skia.so | grep '.*\sT\s.*$$' > $@
 
 $(L).clean:
 	rm -rf $(builddir)
