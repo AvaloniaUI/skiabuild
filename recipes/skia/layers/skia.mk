4b4e21e40a9446184df299f997e0145ce7152e8d
James Walmsley <james@fullfat-fs.co.uk>
2022-05-02 17:07:28 +0100

    [WASM] Initial WASM build.

--- recipes/skia/layers/skia.mk
+++ recipes/skia/layers/skia.mk
@@ -8,6 +8,10 @@ skia_sync_deps:=$(LSTAMP)/skia_sync_deps
 skia_config:=$(LSTAMP)/skia_config
 skia_install:=$(LSTAMP)/skia_install
 
+ifeq ($(CONFIG_SKIA_EMBEDDED_FONTS),y)
+skia_embedded_fonts:=$(LSTAMP)/skia_embedded_fonts
+endif
+
 ifndef WINDOWS
 skia_pkgconfig:=$(SYSROOT)/$(PREFIX)/$(LIBDIR)/pkgconfig/skia.pc $(PKGROOT)/$(PREFIX)/$(LIBDIR)/pkgconfig/skia.pc
 endif
@@ -17,6 +21,9 @@ $(call git_clone, skia, https://github.com/google/skia.git, $(SKIA_GIT_REF))
 $(L) += $(skia_sync_deps)
 $(L) += $(skia_config)
 $(L) += $(skia)
+ifeq ($(CONFIG_SKIA_EMBEDDED_FONTS),y)
+$(L) += $(skia_embedded_fonts)
+endif
 $(L) += $(skia_install)
 $(L) += $(skia_pkgconfig)
 
@@ -49,20 +56,42 @@ ifdef WINDOWS
 GN:=./bin/gn
 endif
 
+TC_SOURCE:=
+
+ifeq ($(CONFIG_VARIANT),wasm)
+TC_SOURCE=source $(srcdir)/skia/third_party/externals/emsdk/emsdk_env.sh &&
+SKIA_TARGET:=libskia.a
+endif
+
 $(skia_config): $(skia_sync_deps)
-	cd $(srcdir)/skia && $(GN) gen $(builddir)/skia --args="$(SKIA_ARGS)"
+	cd $(srcdir)/skia && $(TC_SOURCE) $(GN) gen $(builddir)/skia --args="$(SKIA_ARGS)"
 	$(stamp)
 
 $(skia): $(skia_config)
-	cd $(builddir)/skia && ninja $(BUILD_JOBS)
+	cd $(builddir)/skia && ninja $(BUILD_JOBS) $(SKIA_TARGET)
 	$(stamp)
 
+ifeq ($(CONFIG_SKIA_EMBEDDED_FONTS),y)
+$(skia_embedded_fonts): $(skia)
+	mkdir -p $(builddir)/merge-dir
+	cd $(builddir)/merge-dir && $(TC_SOURCE) for file in $$(ls $(builddir)/skia/*.a) ; do \
+		$(SKIA_AR) x $$file ; \
+	done
 
-$(skia_install): $(skia)
+	cd $(builddir)/skia && $(srcdir)/skia/tools/embed_resources.py --name SK_EMBEDDED_FONTS --input $(srcdir)/skia/modules/canvaskit/fonts/NotoMono-Regular.ttf --output $(builddir)/font.cpp --align 4
+	cd $(builddir)/merge-dir && $(TC_SOURCE) $(SKIA_CXX) -std=c++17 -I. $(builddir)/font.cpp -r -o $(builddir)/merge-dir/font.o
+
+	$(TC_SOURCE) $(SKIA_AR) -crs $(builddir)/skia/libskia.a $(builddir)/merge-dir/*.o
+
+	cd $(builddir)/skia && $(TC_SOURCE) ninja skia_c_api_example
+	$(stamp)
+endif
+
+$(skia_install): $(skia) $(skia_embedded_fonts)
 ifndef WINDOWS
 	mkdir -p $(SYSROOT)/$(PREFIX)/include/skia
 	mkdir -p $(PKGROOT)/$(PREFIX)/include/skia
-	mkdir -p $(PKGROOT)/$(PREFIX)/$(LIBDIR)
+	mkdir -p $(SYSROOT)/$(PREFIX)/$(LIBDIR)/pkgconfig
 	mkdir -p $(PKGROOT)/$(PREFIX)/$(LIBDIR)/pkgconfig
 	cp -rv $(srcdir)/skia/include $(SYSROOT)/$(PREFIX)/include/skia
 	cp -rv $(srcdir)/skia/include $(PKGROOT)/$(PREFIX)/include/skia
